e44d9e6d680ace4e64165a6458b1570f4385f303
gxgbxgggbx <76139114+gxgbxgggbx@users.noreply.github.com>
2024-01-28 04:04:33 +0000

    add js n stuff

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-var cards = document.querySelectorAll('.card');
+var currentBoard = [["","","","","",""],
+                    ["","","","","",""],
+                    ["","","","","",""]]
 
-[...cards].forEach((card)=>{
-  card.addEventListener( 'click', function() {
-    card.classList.toggle('is-flipped');
-  });
-});
+
+
+var possibleBoards = [[ ["f","s","u","f","c","m"],
+                        ["v","m","c","u","m","v"],
+                        ["s","f","s","m","f","s"]],
+
+                       [["u","m","v","m","f","s"],
+                        ["m","v","s","c","c","f"],
+                        ["s","u","f","m","f","s"]]]
+
+function findBoards(currentBoard, possibleBoards){
+    var foundBoards = possibleBoards
+    for(lineIndex in currentBoard){
+        var line = currentBoard[lineIndex]
+        console.log(line)
+        for(charIndex in line){
+            char = line[charIndex]
+            if(char != ""){
+                tempBoards = foundBoards
+                for(boardIndex in foundBoards){
+                    board = foundBoards[boardIndex]
+                    if(board[lineIndex][charIndex] != char){
+                        tempBoards = tempBoards.filter(function(e) { return e !== board })
+                        if(tempBoards.length == 0){
+                            return []
+                        }
+                    }
+                }
+                foundBoards = tempBoards
+            }
+        }
+    } 
+    return foundBoards
+}
+
